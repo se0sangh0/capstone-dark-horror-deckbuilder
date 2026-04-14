f08a3d53c8a0687d7944b70c05d3e32e9938be5e
No-Angry <ashyeon0422@gmail.com>
2026-04-14 10:41:54 +0900

    feat: 병합을 위한 커밋(Cost값 및 재화 관련 스크립트 작성 완료, GameManager와 연동 필요)

---
+++ Assets/Scripts/Currency/ManastoneManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+
+public class ManastoneManager : BaseCurrency
+{
+    [SerializeField] private TMP_Text amountText;
+    public static ManastoneManager Instance { get; private set; }
+
+    protected override int StartingAmount => 10;
+
+    // 부모가 요구하는 저장 키 값 정의 (여기만 다름!)
+    protected override string SaveKey => "ManaStone";
+
+    private void Awake()
+    {
+        OnCurrencyChanged += UpdateText;
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void UpdateText(int amount)
+    {
+        if (amountText is null) return;
+
+        amountText.text = $"{amount:N0}";
+    }
+}
